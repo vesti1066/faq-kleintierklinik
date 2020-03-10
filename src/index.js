f9dd79da50f1b7b0a0530f4965ef529c15399b57
Steve Maibach <steve.maibach@heig-vd.ch>
2020-03-10 13:39:46 +0100

    init files ready to code

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'css/wow.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: false,
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      { 
+        test:/\.css$/,
+        use: [ 'style-loader', {
+          loader: 'css-loader',
+          options: {url: false} 
+        }],        
+      } 
+    ]
+  },
+  resolve: {
+    modules: [
+      path.resolve('./src'),
+      path.resolve('./node_modules')
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    })
+  ]
+};
