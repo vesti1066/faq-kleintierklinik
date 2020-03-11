fae77e4cdff5bfc3ad7c983360bd4af8bf15131a
Steve Maibach <steve.maibach@heig-vd.ch>
2020-03-11 16:50:40 +0100

    redirection and different page

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+  $('.card.bg-success').mouseenter(function( event ) {
+    let current = event.currentTarget;
+    $( current ).children().eq(0).addClass("hidden");
+    $( current ).children().eq(1).removeClass('hidden');
+  });
 
+  $('.card.bg-success').mouseleave(function( event ) {
+    let current = event.currentTarget;
+    $( current ).children().eq(1).addClass("hidden");
+    $( current ).children().eq(0).removeClass('hidden');
+  });
+
+  $('.card.bg-success').click(function (event) {
+     let id = $(event.currentTarget).attr('id');
+     console.log(id);
+    window.location = id+'.html';
+  })
+
+  $('#previous').click(function (event) {
+    history.back();
+  });
 
